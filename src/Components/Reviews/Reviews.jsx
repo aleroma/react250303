1da80317042404753dcea21792b7ab2a9d7c4a24
Roman Alekseev <alekseev-r-v@ya.ru>
2025-03-26 20:39:05 +0300

    Подправил Reviews.jsx

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,11 +1,11 @@
-import {ReviewList} from "./ReviewList";
+import {ReviewList} from "./ReviewList.jsx";
 import {ReviewForm} from "./ReviewForm.jsx";
 
 export const Reviews = ({reviews, activeId}) => {
     return (
         <div className="RestaurantReviews">
             <h3>Reviews:</h3>
-            {reviews.length ? <ReviewList reviews={reviews}/> : 'There are no reviews yet'}
+            {reviews?.length ? <ReviewList reviews={reviews}/> : 'There are no reviews yet'}
             <ReviewForm activeId={activeId} />
         </div>
     )
